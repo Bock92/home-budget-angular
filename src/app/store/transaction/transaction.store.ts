e04dc4a1863336bb6e199bf398d453546befab6b
Simone Boccato <boccato92@gmail.com>
2025-06-24 21:49:39 +0200

    feat(store): add transaction api

--- src/app/store/transaction/transaction.store.ts
+++ src/app/store/transaction/transaction.store.ts
@@ -3,8 +3,10 @@ import { initialTransactionState } from './transaction.state';
 import { inject } from '@angular/core';
 import { TransactionService } from '../../services/transaction.service';
 import { lastValueFrom } from 'rxjs';
+import { TransactionRequestPayload } from '../../model/transaction.model';
 
-export const transactionStore = signalStore(
+export const TransactionStore = signalStore(
+  { providedIn: 'root' },
   withState(initialTransactionState),
   withMethods((store, transactionService = inject(TransactionService)) => {
     const loadTransactions = async () => {
@@ -13,8 +15,20 @@ export const transactionStore = signalStore(
         data: result.data,
       });
     };
+
+    const addTransaction = async (data: TransactionRequestPayload) => {
+      const result = await lastValueFrom(
+        transactionService.addTransaction(data)
+      );
+      if (result) {
+        loadTransactions();
+      }
+    };
     return {
       loadTransactions,
+      addTransaction,
     };
   })
 );
+
+export type TransactionStore = InstanceType<typeof TransactionStore>;
