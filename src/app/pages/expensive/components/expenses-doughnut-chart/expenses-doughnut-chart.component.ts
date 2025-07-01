0db2f34b6716deab6d29731a376dc3a0af953839
Simone Boccato <boccato92@gmail.com>
2025-07-01 23:03:27 +0200

    feat(expenses-doughnut-chart): conntect to real api

--- src/app/pages/expensive/components/expenses-doughnut-chart/expenses-doughnut-chart.component.ts
+++ src/app/pages/expensive/components/expenses-doughnut-chart/expenses-doughnut-chart.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ChartModule } from 'primeng/chart';
+import { Transaction, TransactionStore } from '../../../../store';
 
 @Component({
   selector: 'app-expenses-doughnut-chart',
@@ -8,36 +9,68 @@ import { ChartModule } from 'primeng/chart';
   imports: [ChartModule, CardModule],
 })
 export class ExpensesDoughnutChartComponent {
-  data = {
-    labels: ['House', 'Insurance', 'Bill'],
-    datasets: [
-      {
-        data: [300, 50, 100],
-        backgroundColor: [
-          getComputedStyle(document.documentElement).getPropertyValue(
-            '--p-cyan-500'
-          ),
-          getComputedStyle(document.documentElement).getPropertyValue(
-            '--p-orange-500'
-          ),
-          getComputedStyle(document.documentElement).getPropertyValue(
-            '--p-gray-500'
-          ),
-        ],
-        hoverBackgroundColor: [
-          getComputedStyle(document.documentElement).getPropertyValue(
-            '--p-cyan-400'
-          ),
-          getComputedStyle(document.documentElement).getPropertyValue(
-            '--p-orange-400'
-          ),
-          getComputedStyle(document.documentElement).getPropertyValue(
-            '--p-gray-400'
-          ),
-        ],
+  readonly #transactionStore = inject(TransactionStore);
+  readonly #labels = computed(() => {
+    console.log('labels');
+    const transactions = this.#transactionStore.data();
+    return Array.from(
+      new Set(transactions.map((transaction) => transaction.category.name))
+    );
+  });
+
+  readonly #dataList = computed(() => {
+    const labels = this.#labels();
+    const transactions = this.#transactionStore.data();
+
+    const totals = labels.reduce(
+      (acc: { [key: string]: number }, label: string) => {
+        acc[label] = 0;
+        return acc;
       },
-    ],
-  };
+      {}
+    );
+    transactions.forEach((transaction) => {
+      totals[transaction.category.name] =
+        totals[transaction.category.name] + transaction.amount;
+    });
+
+    console.log(totals);
+    return Object.values(totals);
+  });
+
+  $data = computed(() => {
+    return {
+      labels: this.#labels(),
+      datasets: [
+        {
+          data: this.#dataList(),
+          backgroundColor: [
+            getComputedStyle(document.documentElement).getPropertyValue(
+              '--p-cyan-500'
+            ),
+            getComputedStyle(document.documentElement).getPropertyValue(
+              '--p-orange-500'
+            ),
+            getComputedStyle(document.documentElement).getPropertyValue(
+              '--p-gray-500'
+            ),
+          ],
+          hoverBackgroundColor: [
+            getComputedStyle(document.documentElement).getPropertyValue(
+              '--p-cyan-400'
+            ),
+            getComputedStyle(document.documentElement).getPropertyValue(
+              '--p-orange-400'
+            ),
+            getComputedStyle(document.documentElement).getPropertyValue(
+              '--p-gray-400'
+            ),
+          ],
+        },
+      ],
+    };
+  });
+
   options = {
     cutout: '90%',
     plugins: {
